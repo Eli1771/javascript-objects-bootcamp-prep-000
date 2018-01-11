fb43a452e27944a69e1350d3adf17cfdae9ffc5d
elijahmm17@gmail.com <elijahmm17@gmail.com>
2018-01-11 16:28:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(object, artist, song) {
   object.artist = song;
   return object;
 }
+
+function removeFromPlaylist(object, artist) {
+  delete object.artist;
+  return object;
+}
